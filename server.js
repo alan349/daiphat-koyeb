04264dded8cb4a771c6f2fe62bf9d516003a4fdb
alan <168492923+alan349@users.noreply.github.com>
2025-07-16 11:59:44 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,15 +1,13 @@
 const express = require("express");
-const app = express();
 const path = require("path");
+const app = express();
+const PORT = process.env.PORT || 8080;
 
-const PORT = process.env.PORT || 3000;
-
-// phục vụ file tĩnh nếu bạn có thêm CSS, JS...
+// phục vụ file tĩnh nếu có thêm CSS, JS
 app.use(express.static(__dirname));
 
-// trả về index.html đúng MIME type
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "index.html"));
+  res.sendFile(path.join(__dirname, "index.html")); // quan trọng!
 });
 
 app.listen(PORT, () => {
